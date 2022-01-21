6ee7950e46e480782ba247842430273ca0244fc0
verma-divyanshu-git <96394150+verma-divyanshu-git@users.noreply.github.com>
2022-01-21 21:49:52 +0530

    Create Assignment2_21103104.py

---
+++ Assignment2_21103104.py
@@ -0,0 +1,86 @@
+# Name - Divyanshu
+# SID - 21103104
+
+
+# QUESTION 1
+string_1="Python is a case sensitive language" 
+print("<a>Length of the string is : ",len(string_1)) #function to find LENGTH OF STRING
+print("<b>The reversed string is : ",string_1[-1::-1])
+# <c>
+string_2=string_1[10:26] #Stored "a case sensitive" in a new string
+# <d>
+string_2.replace("a case sensitive","object oriented") #REPLACED "a case sensitive" WITH "object oriented"
+print("<e>THE INDEX OF SUBSTRING a is : ",string_1.find('a'))
+print("<f>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS : ",string_1.replace(" ",""))
+print("\n")
+
+
+
+#QUESTION 2
+NAME=input("ENTER YOUR NAME")
+SID=int(input("ENTER YOUR SID"))
+DEPARTMENT=input("ENTER YOUR DEPARTMENT")
+CGPA=float(input("ENTER YOUR CGPA"))
+print("Hey %s,"%NAME,"Here!")
+print("My SID is %d" %SID)
+print("I am from %s"%DEPARTMENT,"and my CGPA is %f"%CGPA)
+print("\n")
+
+
+
+#QUESTION 3
+a=56
+b=10
+#a
+print("a. ",a&b)
+#b
+print("b. ",a|b)
+#c
+print("c. ",a^b)
+#d
+print("Left shift both a and b with 2 bits respectively are :",a<<2 ,b<<2)
+#e
+print("Right shift a with 2 bits and b with 4 bits respectively are : ",a>>2,b>>4)
+print("\n")
+
+
+
+#QUESTION 4
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+number_3 = int(input("Enter third number: "))
+ 
+if (number_1 > number_2) and (number_1 > number_3):
+   largest = number_1
+elif (number_2 > number_1) and (number_2 > number_3):
+   largest = number_2
+else:
+   largest = number_3
+ 
+print("THE LARGEST NUMBER IS ",largest)
+print("\n")
+
+
+
+#QUESTION 5
+word_present=input("ENTER A STRING")
+if (word_present.find('name') != -1):
+    print ("Yes")
+else:
+    print ("No")
+print("\n")
+
+
+#QUESTION 6
+side_1=int(input("ENTER LENGTH OF FIRST SIDE OF TRIANGLE : "))
+side_2=int(input("ENTER LENGTH OF SECOND SIDE OF TRIANGLE : "))
+side_3=int(input("ENTER LENGTH OF THIRD SIDE OF TRIANGLE : "))
+if(side_1>(side_2+side_3)):
+    print("No")
+elif(side_2>(side_1+side_2)):
+    print("No")
+elif(side_3>(side_1+side_2)):
+    print("No")
+else:
+    print("Yes")
+print("\n")
