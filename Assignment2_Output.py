84d39260687034fcf313526631383f5fef6b53ee
verma-divyanshu-git <96394150+verma-divyanshu-git@users.noreply.github.com>
2022-01-21 21:56:52 +0530

    Output_Files_Assgn2

---
+++ Assignment2_Output.py
@@ -0,0 +1,47 @@
+# NAME - Divyanshu
+# SID - 21103104
+
+
+#Output1
+<a>Length of the string is :  35
+<b>The reversed string is :  egaugnal evitisnes esac a si nohtyP
+<e>THE INDEX OF SUBSTRING a is :  10
+<f>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS :  Pythonisacasesensitivelanguage
+  
+
+  
+#Output2  
+ENTER YOUR NAMEDivyanshu
+ENTER YOUR SID21103104
+ENTER YOUR DEPARTMENTCSE
+ENTER YOUR CGPA9.9
+Hey Divyanshu, Here!
+My SID is 21103104
+I am from CSE and my CGPA is 9.900000
+
+
+#Output3
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+  
+
+#Output4
+Enter first number: 50
+Enter second number: 20
+Enter third number: 35
+THE LARGEST NUMBER IS  50
+
+
+#Output5
+ENTER A STRINGHelloWorld
+No
+
+
+#Output6
+ENTER LENGTH OF FIRST SIDE OF TRIANGLE : 39
+ENTER LENGTH OF SECOND SIDE OF TRIANGLE : 40
+ENTER LENGTH OF THIRD SIDE OF TRIANGLE : 41
+Yes
