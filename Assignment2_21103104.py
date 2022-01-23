461b8e966ad3354dcc88907e1694dc8549521ec2
verma-divyanshu-git <96394150+verma-divyanshu-git@users.noreply.github.com>
2022-01-24 03:10:47 +0530

    Update Assignment2_21103104.py

--- Assignment2_21103104.py
+++ Assignment2_21103104.py
@@ -17,10 +17,10 @@ print("\n")
 
 
 #QUESTION 2
-NAME=input("ENTER YOUR NAME")
-SID=int(input("ENTER YOUR SID"))
-DEPARTMENT=input("ENTER YOUR DEPARTMENT")
-CGPA=float(input("ENTER YOUR CGPA"))
+NAME=input("ENTER YOUR NAME ")
+SID=int(input("ENTER YOUR SID "))
+DEPARTMENT=input("ENTER YOUR DEPARTMENT ")
+CGPA=float(input("ENTER YOUR CGPA "))
 print("Hey %s,"%NAME,"Here!")
 print("My SID is %d" %SID)
 print("I am from %s"%DEPARTMENT,"and my CGPA is %f"%CGPA)
