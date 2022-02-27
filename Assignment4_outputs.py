a9444bac03edc6286e4ee87fb00a3ea2e7365d41
verma-divyanshu-git <96394150+verma-divyanshu-git@users.noreply.github.com>
2022-02-27 13:27:39 +0530

    Output4_21103104

---
+++ Assignment4_outputs.py
@@ -0,0 +1,61 @@
+Ans 1
+Move disk 1 from source A to destination C
+Move disk 2 from source A to destination B
+Move disk 1 from source C to destination B
+Move disk 3 from source A to destination C
+Move disk 1 from source B to destination A
+Move disk 2 from source B to destination C
+Move disk 1 from source A to destination C
+
+
+
+Ans 2
+Enter the size of Pascal’s triangle: 5
+Solution using loops: 
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+
+
+Solution using recursion: 
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+
+
+Ans 3
+Enter 2 numbers: 5 3
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> All the values are non zero
+<c> Filtered out numbers that are greater than 4 are : [5, 6, 7, 6, 7, 7, 8]
+<d> Set: {8, 5, 6, 7}
+<e> Immutable set: frozenset({8, 5, 6, 7})
+<f> Hash value of the max value from the above set: 8
+
+
+Ans 4 
+Object created
+The name of the student is Divyanshu and SID is 21103104.
+Object destroyed
+
+
+
+Ans 5
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
+
+
+Ans 6
+Enter the word: fare
+Enter a new meaningful word using the exact same letters to test your friendship: fear
+Does the word makes sense?(y/Y or n/N )y
+The friends pass the friendship test
